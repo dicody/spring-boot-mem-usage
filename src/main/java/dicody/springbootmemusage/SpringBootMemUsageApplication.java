60d348319f4601f33a42ab4716641600feb223c9
dicody <dmitrijs.rahmanins@gmail.com>
2022-08-16 11:03:24 +0300

    spring boot proj initialized

---
+++ src/main/java/dicody/springbootmemusage/SpringBootMemUsageApplication.java
@@ -0,0 +1,13 @@
+package dicody.springbootmemusage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMemUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMemUsageApplication.class, args);
+	}
+
+}
